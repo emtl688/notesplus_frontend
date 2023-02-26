b270b7f815b11b8308ef2b29eb968c14fa77a031
emtl688 <ericpmoran1@gmail.com>
2023-02-26 17:09:49 -0500

    checkpoint

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,32 +1,46 @@
 import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import useTitle from "../../hooks/useTitle";
+import { Box, Grid } from "@mui/material";
 
 const Welcome = () => {
+  useTitle("Home");
+
   const { username, isManager, isAdmin } = useAuth();
 
   const content = (
     <section className="welcome">
-      <h1 className="welcome__user">Welcome {username} !</h1>
-      
-      <p>
-        <Link to="/dash/notes">View Open Notes</Link>
-      </p>
-
-      <p>
-        <Link to="/dash/notes/new">Add New Note</Link>
-      </p>
-
-      {(isManager || isAdmin) && (
-        <p>
-          <Link to="/dash/users">View User Settings</Link>
-        </p>
-      )}
-
-      {(isManager || isAdmin) && (
-        <p>
-          <Link to="/dash/users/new">Add New User</Link>
-        </p>
-      )}
+      <header>
+        <h1 className="welcome__user">Welcome {username} !</h1>
+      </header>
+      <main style={{ flexDirection: "row", width: "100%", paddingTop: "10px" }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={8}>
+            {/* TASKS / NOTES */}
+            <Grid item xs={3}>
+              <h3 className="welcome__subtitle">Notes</h3>
+              <div className="welcome__sublink">
+                <Link to="/dash/notes">View Open Notes</Link>
+              </div>
+              <div className="welcome__sublink">
+                <Link to="/dash/notes/new">Add New Note</Link>
+              </div>
+            </Grid>
+            {/* USERS (ADMINS ONLY) */}
+            {(isManager || isAdmin) && (
+              <Grid item xs={3}>
+                <h3 className="welcome__subtitle">Users</h3>
+                <div className="welcome__sublink">
+                  <Link to="/dash/users">View User Settings</Link>
+                </div>
+                <div className="welcome__sublink">
+                  <Link to="/dash/users/new">Add New User</Link>
+                </div>
+              </Grid>
+            )}
+          </Grid>
+        </Box>
+      </main>
     </section>
   );
 
