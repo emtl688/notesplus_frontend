05feaee525821377b6b0dad123f49470ed829e0c
emtl688 <ericpmoran1@gmail.com>
2023-02-27 17:52:35 -0500

    pre deployment changes

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500",
+  baseUrl: "https://mycrm-ericm-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
@@ -15,13 +15,8 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-  // console.log(args) // request url, method, body
-  // console.log(api) // signal, dispatch, getState()
-  // console.log(extraOptions) //custom like {shout: true}
-
   let result = await baseQuery(args, api, extraOptions);
 
-  // If you want, handle other status codes, too
   if (result?.error?.status === 403) {
     console.log("sending refresh token");
 
