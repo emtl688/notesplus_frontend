c6657212ad88513a48348fb7cf686cc4d3097842
emtl688 <ericpmoran1@gmail.com>
2023-02-28 00:47:44 -0500

    revert to default

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,18 +2,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://mycrm-ericm-api.onrender.com",
-  mode: "no-cors",
-  credentials: "include",
+  baseUrl: 'https://mycrm-ericm-api.onrender.com',
+  credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token;
+      const token = getState().auth.token
 
-    if (token) {
-      headers.set("authorization", `Bearer ${token}`);
-    }
-    return headers;
-  },
-});
+      if (token) {
+          headers.set("authorization", `Bearer ${token}`)
+      }
+      return headers
+  }
+})
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
