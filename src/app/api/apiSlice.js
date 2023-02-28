2b83deb51ddd079bde4e026b3d58deadb002b9ac
emtl688 <ericpmoran1@gmail.com>
2023-02-28 00:07:45 -0500

    try cors fix

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,6 +3,7 @@ import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://mycrm-ericm-api.onrender.com",
+  mode: "cors",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
