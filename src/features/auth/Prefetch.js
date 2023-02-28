b7f7c5c73735b3bc52395eac716a79cbc05a105b
emtl688 <ericpmoran1@gmail.com>
2023-02-28 01:33:32 -0500

    added customers to prefetch

--- src/features/auth/Prefetch.js
+++ src/features/auth/Prefetch.js
@@ -1,6 +1,7 @@
 import { store } from "../../app/store";
 import { usersApiSlice } from "../users/usersApiSlice";
 import { tasksApiSlice } from "../tasks/tasksApiSlice";
+import { customersApiSlice } from "../customers/customersApiSlice";
 import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
@@ -8,6 +9,7 @@ const Prefetch = () => {
   useEffect(() => {
     store.dispatch(usersApiSlice.util.prefetch("getUsers", "usersList", { force: true }));
     store.dispatch(tasksApiSlice.util.prefetch("getTasks", "tasksList", { force: true }));
+    store.dispatch(customersApiSlice.util.prefetch("getCustomers", "customersList", { force: true }));
   }, []);
 
   return <Outlet />;
